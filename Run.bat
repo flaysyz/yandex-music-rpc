60fb143bc8d03107860f0239b6b0253315447234
flaysyz <91736204+flaysyz@users.noreply.github.com>
2023-09-10 17:10:47 +0300

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,5 +1,5 @@
 @echo off
-cd C:\Users\georg\OneDrive\Documents\yandis
-python "C:\Users\georg\OneDrive\Documents\yandis\main.py"
+cd Вставь сюда путь к папке со скриптом например: C:\Users\user\OneDrive\Documents\yandex-music-rpc !!! Без кавычек
+python "вставь сюда путь к файлу main.py например: "C:\Users\user\OneDrive\Documents\yandex-music-rpc\main.py" !!! Нужно оставить кавычки"
 
 pause
